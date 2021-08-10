943dc5b83b150cb8955f0be585c060e38b3913b6
Saakshi-2k <saakshi.2k@gmail.com>
2021-08-10 18:06:08 +0530

    Angular UI Product Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/common/header/header.component';
+import { SearchComponent } from './components/common/search/search.component';
+import { FooterComponent } from './components/common/footer/footer.component';
+import { FilterComponent } from './components/category/filter/filter.component';
+import { ProductsComponent } from './components/category/products/products.component';
+import { CategoryComponent } from './components/category/category.component';
+import { SortComponent } from './components/category/sort/sort.component';
+import { ItemsComponent } from './components/category/products/items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    FooterComponent,
+    FilterComponent,
+    ProductsComponent,
+    CategoryComponent,
+    SortComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule,
